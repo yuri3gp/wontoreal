c1a4948411147b97afa486c282b7f3a03eaab320
yuri3gp <yuri3gp@gmail.com>
2024-12-29 15:41:24 -0300

    won to real

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Won to Real" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
